bdfbf64c5e43ac06382d506ca684f5bbad99a216
Nicolas Klein <ncl.klein@gmail.com>
2015-04-18 16:39:31 +0200

    Storing new places as tags of cat 2 into DS Photo

--- places.py
+++ places.py
@@ -1,5 +1,50 @@
+import psycopg2
+import psycopg2.extras
+import json
+
 _knownPlaces = dict()
 _placeIds = 0
+
+class Place:
+    def __init__(self, id, parts):
+        self._id = id
+        self.parts = _make_unique(parts)
+        self.path = ','.join(self.parts)
+        self._name = parts[0]
+    
+    def __str__(self):
+        return "%s[%d]" % (self.path, self._id)
+
+
+class Geotags:
+    def __init__(self, db):
+        self._knownPlaces = dict()
+        self.db = db
+        self.cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        self.cursor.execute("""SELECT id, name, info FROM photo_label WHERE category=2""")
+        for tag in self.cursor:
+            if len(tag['info']) > 0:
+                 info = json.loads(tag['info'])
+                 place = Place(tag['id'], info['parts'])
+                 _knownPlaces[place.path] = place
+    
+    def place(self, parts):
+        uniqueParts = _make_unique(parts)
+        key = ','.join(uniqueParts)
+        if key in self._knownPlaces:
+            return self._knownPlaces[key]
+        return self._newPlace(uniqueParts)
+    
+    def _newPlace(self, parts):
+        info = json.dumps({'parts': parts})
+        self.cursor.execute("""INSERT INTO photo_label (category, name, info)
+                               VALUES (%(category)s, %(name)s, %(info)s) RETURNING id""", 
+                               {'category': 2, 'name': info[0], 'info': info})
+        id = self.cursor.fetchone()['id']
+        self.db.commit()
+        place = Place(id, parts)
+        self._knownPlaces[place.path] = place
+        return place
 
 def _make_unique(items):
     result = []
@@ -7,17 +52,6 @@ def _make_unique(items):
         if i != None and i not in result:
             result.append(i)
     return result
-
-class Place:
-    def __init__(self, id, parts):
-        self.id = id
-        self.parts = _make_unique(parts)
-        self.path = ','.join(self.parts)
-        self.name = parts[0]
-    
-    def __str__(self):
-        return "%s[%d]" % (self.path, self.id)
-    
 
 def place(parts):
     global _placeIds
@@ -38,6 +72,7 @@ if __name__ == "__main__":
     p2 = place(('Obernai', 'Alsace', 'France'))
     print p1
     print p2
-
+    conn = psycopg2.connect("dbname=photo user=postgres")
+    geotags = Geotags(conn)
 
 
